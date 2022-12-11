e5368b98101d30f33de61417c9bec6d76372b8ab
adbeglov <HUDAIDOTT@yandex.ru>
2022-12-11 17:39:28 +0300

    addind Epic,  SubTask, TaskManager

---
+++ oop_project/oop_project/managers/InMemoryTaskManager.cs
@@ -0,0 +1,52 @@
+﻿using oop_project.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_project.managers
+{
+    class InMemoryTaskManager : TaskManager
+    {
+        public void clearTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int createNewTask(model.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Subtask> getAllSubtasksOfEpicByEpicId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<model.Task> getListOfAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<model.Task> getPrioritizedTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public model.Task getTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateTask(model.Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ oop_project/oop_project/model/Subtask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_project.model
+{
+    class Subtask: Task
+    {
+        private int epicId;
+
+        public Subtask(string name, string description, Status status, int epicId) : base(name, description, status)
+        {
+            this.EpicId = epicId;
+        }
+
+        public Subtask(int id, string name, string description, Status status, int epicId) : base(id, name, description, status)
+        {
+            this.EpicId = epicId;
+        }
+
+        public Subtask(string name, string description, Status status, int duration, DateTime deadline, int epicId) : base(name, description, status, duration, deadline)
+        {
+            this.EpicId = epicId;
+        }
+
+        public Subtask(int id, string name, string description, Status status, int duration, DateTime deadline, int epicId) : base(id, name, description, status, duration, deadline)
+        {
+            this.EpicId = epicId;
+        }
+
+        public int EpicId { get => epicId; set => epicId = value; }
+    }
+}
